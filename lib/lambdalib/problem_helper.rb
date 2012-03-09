4a1daf8e51643139ee5b15ef6141f3d3c465e156
Greg Belote <etoleb@gmail.com>
2012-03-09 17:22:53 -0500

    tweak

--- lib/lambdalib/problem_helper.rb
+++ lib/lambdalib/problem_helper.rb
@@ -1,4 +1,9 @@
-ENV["RAILS_ENV"] = "test"
+# disabling now, because of DB testing...
+#ENV["RAILS_ENV"] = "test"
 require 'config/environment'
 require 'colored'
+
+def assert(expression)
+  throw AssertionError unless expression
+end
 
