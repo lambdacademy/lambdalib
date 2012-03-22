0c5bbbc129b077adbbf0b5d9bd06c76903c3b893
Greg Belote <etoleb@gmail.com>
2012-03-22 05:47:11 -0400

    fix typo

--- lib/lambdalib.rb
+++ lib/lambdalib.rb
@@ -1,5 +1,5 @@
 require "lambdalib/version"
-require "lambdalib/ASCII_LOGO"
+require "lambdalib/ascii_logo"
 require "lambdalib/railtie" if defined? Rails
 require "lambdalib/problem"
 require "lambdalib/pset"
